bbbc65371f889a2bd7baf0e31739cf035246d3cc
Herman Hong <hyong_cs@outlook.com>
2024-04-27 18:40:44 +0800

    refactor(struct): 更新项目结构

---
+++ views/test/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import '../../src/assets/global.css'
+import App from '../../src/views/test/App.vue'
+
+createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        test: resolve(__dirname, 'views/test/index.html')
+      }
+    }
+  }
 })
