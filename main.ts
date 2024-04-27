cd0ce94d0c24a16ba4fb47efd3a381b2f02f346e
Herman Hong <hyong_cs@outlook.com>
2024-04-27 22:29:12 +0800

    refactor(struct): 改一下项目结构

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './src/assets/global.css'
-import App from './src/views/App.vue'
+import { createApp } from "vue";
+import "./src/assets/global.css";
+import App from "./src/views/main/App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
