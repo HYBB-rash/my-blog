a38bc2447f8bf8f7f021362f72fbc331f2d96312
Herman Hong <hyong_cs@outlook.com>
2024-05-04 11:03:48 +0800

    refactor(struct): 整理一下

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,9 +1,9 @@
 pub mod index {
 
-    use crate::views::hello::get_hello;
+    use crate::views::tmpl;
     use actix_web::{get, web, Responder};
     #[get("/index/{name}")]
     async fn greet(name: web::Path<String>) -> impl Responder {
-        get_hello(&name)
+        tmpl(&name)
     }
 }
